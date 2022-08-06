170a60e174172e37bbefae6d69bc78c9a3911f76
Chen Pang <echevil@gmail.com>
2022-08-05 23:12:48 -0700

    Update script to remove dryrun

--- system/scripts/ssl_renew.sh
+++ system/scripts/ssl_renew.sh
@@ -4,5 +4,5 @@ COMPOSE="/usr/local/bin/docker-compose --no-ansi -f docker-compose.prod.yml"
 DOCKER="/usr/bin/docker"
 
 cd /root/code/proj-roselite
-$COMPOSE run certbot renew --dry-run && $COMPOSE kill -s SIGHUP webserver
+$COMPOSE run certbot renew && $COMPOSE kill -s SIGHUP webserver
 $DOCKER system prune -af
