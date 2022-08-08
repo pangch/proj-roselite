9bc3685e7d4153d1069aa10d16628de2607ea0bd
Chen Pang <echevil@gmail.com>
2022-08-07 18:20:39 -0700

    Add info

--- client/app/components/Info.jsx
+++ client/app/components/Info.jsx
@@ -1,5 +1,50 @@
 import * as React from "react";
+import { useState, useEffect } from "react";
+import ShowDetailLink from "./ShowDetailLink";
+
+function DeviceInfoDetails({ info }) {}
+
+function LocalInfoBlock() {
+  const [deviceInfo, setDeviceInfo] = useState({ state: "querying" });
+  useEffect(() => {
+    navigator.mediaDevices
+      .enumerateDevices()
+      .then((info) => {
+        console.log(info);
+        setDeviceInfo({ state: "ready", detail: info });
+      })
+      .catch((err) => {
+        setDeviceInfo({ state: "error", detail: err.message });
+      });
+  }, []);
+  return (
+    <div className="info-block">
+      <div className="info-row">
+        <span className="info-header">Devices</span>
+        <span className="info-value">
+          {deviceInfo.state === "querying" && "Querying..."}
+          {deviceInfo.state === "error" && `Error: ${deviceInfo.detail}`}
+          {deviceInfo.state === "ready" && (
+            <ShowDetailLink
+              label="Show"
+              details={deviceInfo.detail.map((device) => (
+                <pre>{JSON.stringify(device)}</pre>
+              ))}
+            />
+          )}
+        </span>
+      </div>
+    </div>
+  );
+}
 
 export default function Info() {
-  return <section className="section-info grow">Info</section>;
+  return (
+    <section className="section-info grow flex flex-col">
+      <header>Info</header>
+      <div className="overflow-y-scroll basis-0 grow">
+        <LocalInfoBlock />
+      </div>
+    </section>
+  );
 }
---
+++ client/app/components/ShowDetailLink.jsx
@@ -0,0 +1,37 @@
+import * as React from "react";
+import { useState } from "react";
+
+function DetailView({ isShown, setIsShown, details }) {
+  if (!isShown) {
+    return null;
+  }
+  console.log("Here");
+  return (
+    <div className="details-container">
+      <div className="details-content flex flex-col">
+        <header>
+          <i
+            className="fa fa-window-close close-button"
+            onClick={() => setIsShown(false)}
+          ></i>
+        </header>
+        <div className="details-content-body overflow-scroll">{details}</div>
+      </div>
+    </div>
+  );
+}
+
+export default function ShowDetailLink({ label, details }) {
+  const [isShown, setIsShown] = useState(false);
+  const handleOnClick = () => {
+    setIsShown(true);
+  };
+  return (
+    <>
+      <div className="details-link" onClick={handleOnClick}>
+        {label}
+      </div>
+      <DetailView isShown={isShown} setIsShown={setIsShown} details={details} />
+    </>
+  );
+}
