ace4994c4dcfd20806ae7cb9639fe32ec5e0e706
Chen Pang <echevil@gmail.com>
2022-08-09 22:29:57 -0700

    Update title

--- client/app/components/Title.jsx
+++ client/app/components/Title.jsx
@@ -3,7 +3,11 @@ import * as React from "react";
 export default function Title() {
   return (
     <section className="section-title flex">
-      <h2>WebRTC experiments</h2>
+      <div className="title-header grow">WebRTC experiments</div>
+      <div className="title-notes">
+        <div>Text messaging and video conferencing</div>
+        <div>For learning purpose only</div>
+      </div>
     </section>
   );
 }
