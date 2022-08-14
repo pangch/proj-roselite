d9820868302efd1f68f2a5b2b72391be0580cd0a
Chen Pang <echevil@gmail.com>
2022-08-13 23:59:43 -0700

    Updated project path

--- system/scripts/ssl_renew.sh
+++ system/scripts/ssl_renew.sh
@@ -3,6 +3,6 @@
 COMPOSE="/usr/local/bin/docker-compose --no-ansi -f docker-compose.prod.yml"
 DOCKER="/usr/bin/docker"
 
-cd /root/code/proj-roselite
+cd /root/proj-roselite
 $COMPOSE run certbot renew && $COMPOSE kill -s SIGHUP webserver
 $DOCKER system prune -af
